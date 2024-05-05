bd2c4892502ab0936a0e65b66d10a742f3622675
syedamir5560 <syedamir5560@gmail.com>
2024-05-06 00:57:55 +0530

    new

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -10,21 +10,21 @@ const Nav = () => {
 
     return (
         <main className='relative'>
-            <nav className="bg-gray-800 rounded top-5 mx-[2%] left-0 w-[95%] fixed ">
+            <nav className="bg-gray-800 rounded top-5 mx-[2%] left-0 w-[95%] fixed  ">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-                    <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center justify-between h-20">
                         <div className="flex-shrink-0">
-                            <NavLink to='/'> <h2 className="text-white text-2xl font-bold">Trippy</h2></NavLink>
+                            <NavLink to='/'> <h2 className="text-white text-[2rem] font-bold">Trippy</h2></NavLink>
                         </div>
                         <div className="flex items-center">
 
                             <div className="hidden md:block">
-                                <div className="ml-10 flex items-baseline space-x-4 font-bold">
-                                    <NavLink to="/" exact activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm ">Home</NavLink>
-                                    <NavLink to="/about" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</NavLink>
-                                    <NavLink to="/services" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</NavLink>
-                                    <NavLink to="/contact" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</NavLink>
-                                    <NavLink to='/login'><button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login</button></NavLink>
+                                <div className="ml-10 flex items-baseline space-x-4 font-bold ">
+                                    <NavLink to="/" exact activeClassName="text-white" className="text-[1.2rem] text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm "><i class="fa-solid fa-house mx-1"></i> Home</NavLink>
+                                    <NavLink to="/about" activeClassName="text-white" className="text-[1.2rem] text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"><i class="fa-solid fa-circle-info mx-1"></i> About</NavLink>
+                                    <NavLink to="/services" activeClassName="text-white" className="text-[1.2rem] text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"><i class="fa-solid fa-briefcase mx-1"></i> Services</NavLink>
+                                    <NavLink to="/contact" activeClassName="text-white" className="text-[1.2rem] text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"><i class="fa-solid fa-address-book mx-1"></i> Contact</NavLink>
+                                    <NavLink to='/login'><button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 text-[1.2rem]">Sign Up</button></NavLink>
                                 </div>
                             </div>
                         </div>
@@ -41,13 +41,13 @@ const Nav = () => {
                     </div>
                 </div>
                 <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu" >
-                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-center">
+                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-center ">
 
-                        <NavLink to="/" exact activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</NavLink>
-                        <NavLink to="/about" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</NavLink>
-                        <NavLink to="/service" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Services</NavLink>
-                        <NavLink to="/contact" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</NavLink>
-                        <NavLink to="/login" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Login</NavLink>
+                        <NavLink to="/" exact activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"> <i class="fa-solid fa-house mx-2"></i>Home</NavLink>
+                        <NavLink to="/about" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"><i class="fa-solid fa-circle-info    mx-2"></i>About</NavLink>
+                        <NavLink to="/service" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"><i class="fa-solid fa-briefcase mx-2"></i>Services</NavLink>
+                        <NavLink to="/contact" activeClassName="text-white" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"><i class="fa-solid fa-address-book mx-2"></i>Contact</NavLink>
+                        <NavLink to='/login'><button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sign Up</button></NavLink>
                     </div>
                 </div>
             </nav>
