26b646a5b35708b3a4dcf4ae7e0014155a33c748
syedamir5560 <syedamir5560@gmail.com>
2024-05-06 18:01:54 +0530

    footeer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white py-8 px-7 md:py-12">
+            <div className="container mx-auto">
+                <div className="grid grid-cols-1 items-center md:grid-cols-4 gap-10">
+                    <div className="md:col-span-3 md:flex md:justify-between md:mb-4">
+                        <div className='sm-mb-5'>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-2">Project</h3>
+                            <ul className="list-none">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Our Mission</a></li>
+                                <li><a href="#">Features</a></li>
+                            </ul>
+                        </div>
+                        <div className='sm-mb-5'>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-2">Community</h3>
+                            <ul className="list-none">
+                                <li><a href="#">Forum</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Blog</a></li>
+                            </ul>
+                        </div>
+                        <div className='sm-mb-5'    >
+                            <h3 className="text-xl md:text-2xl font-semibold mb-2 ">Help</h3>
+                            <ul className="list-none">
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">FAQs</a></li>
+                                <li><a href="#">Support</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="text-center md:text-left">
+                        <h1 className="text-3xl md:text-5xl  font-bold mb-4">Trippy</h1>
+                        <h2 className="text-lg md:text-2xl mb-4 font-bold">Choose your favorite destination</h2>
+                        <div className="flex justify-center md:justify-start mb-4">
+                            <a href="https://www.facebook.com" className="mr-4"><i class=" fa-brands fa-facebook text-2xl"></i></a>
+                            <a href="https://www.instagram.com" className="mr-4"><i class=" fa-brands fa-instagram text-2xl"></i></a>
+                            <a href="https://www.twitter.com" className="mr-4"><i class=" fa-brands fa-twitter text-2xl"></i></a>
+
+                        </div>
+                        <p>© {new Date().getFullYear()} Trippy. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import Destination from '../Components/Destination'
 import Trip from '../Components/Trip'
+import Footer from '../Components/Footer'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero />
       <Destination />
       <Trip/>
+      <Footer/>
     </div>
   )
 }
