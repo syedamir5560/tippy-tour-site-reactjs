bb3bc7642c8257e77d414330111ca8c7258b18e4
syedamir5560 <syedamir5560@gmail.com>
2024-05-06 16:50:43 +0530

    s

--- src/Components/DestinationData.jsx
+++ src/Components/DestinationData.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DestinationData(props) {
     return (
         <div>
-            <p className="text-lg mb-6">Tour Give The Oppotunity To See Lot Within The Time Frame</p>
+            <p className="text-lg mb-6">{props.para1}</p>
             <h2 className="text-3xl font-bold mb-4">{props.heading}</h2>
             <p className="text-lg mb-20 my-5">{props.para}</p>
             <div className="grid grid-cols-2 gap-10">
