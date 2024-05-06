0f706c5b8b6517152fe866c3d7aa6f5722404e3b
syedamir5560 <syedamir5560@gmail.com>
2024-05-06 20:11:12 +0530

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,12 +6,12 @@ function About() {
       <div className="relative bg-cover bg-center h-[70vh] " style={{ backgroundImage: "url('https://images.unsplash.com/photo-1529171696861-bac785a44828?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg')" }}>
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex flex-col items-center justify-center text-white" style={{ height: '70vh' }}>
-          <h1 className="text-4xl md:text-6xl font-bold text-center">About</h1>
+        
         </div>
       </div>
 
       <div className="container mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-8 font-serif text-center">About Us</h1>
+        <h1 className="text-5xl font-bold mb-8 font-serif text-center ">About Us</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <img src="https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="About Us" className="w-full h-auto mb-4 md:mb-0" />
