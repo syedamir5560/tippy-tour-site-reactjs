c9f69adbb937143db42266828970d6ea3501d256
syedamir5560 <syedamir5560@gmail.com>
2024-05-06 19:44:47 +0530

    s

--- src/Components/Destination.jsx
+++ src/Components/Destination.jsx
@@ -7,7 +7,7 @@ function Destination() {
       <h1 className="text-3xl font-bold mb-4 text-center">Popular Destination</h1>
       <DestinationData heading='Tal Valcano'
       para1='Tour Give The Oppotunity To See Lot Within The Time Frame'
-        para='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, deleniti amet quisquam libero architecto quae, vel corporis voluptatem quasi, accusamus eligendi quibusdam nesciunt inventore unde natus? Commodi ut numquam officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, deleniti amet quisquam libero architecto quae, vel corporis voluptatem quasi, accusamus eligendi quibusdam nesciunt inventore unde natus? Commodi ut numquam officia?'
+        para='vel corporis voluptatem quasi, accusamus eligendi quibusdam nesciunt inventore unde natus? Commodi ut numquam officia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, deleniti amet quisquam libero architecto quae, vel corporis voluptatem quasi, accusamus eligendi quibusdam nesciunt inventore unde natus? Commodi ut numquam officia?'
         image1='https://images.unsplash.com/photo-1557167240-af1446136cbb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         image2='https://images.unsplash.com/photo-1547672920-7732a3b305be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGFsJTIwdmFsY2Fub3xlbnwwfHwwfHx8MA%3D%3D' />
 
